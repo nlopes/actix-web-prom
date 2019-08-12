cabcbf48243e57385a7bbf2c3e38a356d628080e
nWacky <gosha.evtushenko@gmail.com>
2019-08-12 18:47:47 +0300

    Fix test

--- src/lib.rs
+++ src/lib.rs
@@ -126,37 +126,38 @@ use actix_web_prom::PrometheusMetrics;
 use prometheus::Registry;
 use std::thread;
 
-fn test1() -> HttpResponse {
+fn public_handler() -> HttpResponse {
     HttpResponse::Ok().body("Everyone can see it!")
 }
-fn test2() -> HttpResponse {
+
+fn private_handler() -> HttpResponse {
     HttpResponse::Ok().body("This can be hidden behind a firewall")
 }
 
 fn main() -> std::io::Result<()> {
     let shared_registry = Registry::new();
 
-    let private_prom = PrometheusMetrics::new_with_registry(
+    let private_metrics = PrometheusMetrics::new_with_registry(
                                         shared_registry.clone(),
                                         "private_api",
                                         Some("/metrics")
                                     )
                                     // It is safe to unwrap when __no other app has the same namespace__
                                     .unwrap();
-    let public_prom = PrometheusMetrics::new_with_registry(
+    let public_metrics = PrometheusMetrics::new_with_registry(
                                         shared_registry.clone(),
                                         "public_api",
-                                        // Metrics should not be availible from the outside
+                                        // Metrics should not be available from the outside
                                         None
                                     )
                                     .unwrap();
 
 # if false {
-    let public_handle = thread::spawn(move || {
+    let private_thread = thread::spawn(move || {
         HttpServer::new(move || {
             App::new()
-                .wrap(private_prom.clone())
-                .service(web::resource("/test").to(test1))
+                .wrap(private_metrics.clone())
+                .service(web::resource("/test").to(private_handler))
         })
         .bind("127.0.0.1:8081")
         .unwrap()
@@ -164,11 +165,11 @@ fn main() -> std::io::Result<()> {
         .unwrap();
     });
 
-    let private_handle = thread::spawn(|| {
+    let public_thread = thread::spawn(|| {
         HttpServer::new(move || {
             App::new()
-                .wrap(public_prom.clone())
-                .service(web::resource("/test").to(test2))
+                .wrap(public_metrics.clone())
+                .service(web::resource("/test").to(public_handler))
         })
         .bind("127.0.0.1:8082")
         .unwrap()
@@ -176,8 +177,8 @@ fn main() -> std::io::Result<()> {
         .unwrap();
     });
 
-    public_handle.join().unwrap();
-    private_handle.join().unwrap();
+    private_thread.join().unwrap();
+    public_thread.join().unwrap();
 # }
     Ok(())
 }
