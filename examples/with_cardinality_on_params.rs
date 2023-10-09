d025d19fba262bd68743ced35e1b922494c30214
Matthieu Bessat <mail@matthieubessat.fr>
2023-10-09 17:43:20 +0200

    docs: clarify params cardinality example

--- examples/with_cardinality_on_params.rs
+++ examples/with_cardinality_on_params.rs
@@ -1,7 +1,10 @@
 use std::collections::HashMap;
 
+use actix_web::dev::Service;
+use actix_web::HttpMessage;
+
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use actix_web_prom::PrometheusMetricsBuilder;
+use actix_web_prom::{PrometheusMetricsBuilder, MetricsConfig};
 
 
 async fn health() -> HttpResponse {
@@ -27,6 +30,20 @@ async fn main() -> std::io::Result<()> {
             .wrap(prometheus.clone())
             .service(web::resource("/health").to(health))
             .service(
+                web::resource("/services/{service_id}")
+                    .name("Services endpoint")
+                    .wrap_fn(|req, srv| {
+                        // example of a route where we want to keep the details of `service_id` param in the metrics
+                        // we use a middleware to specify that `service_id` param values are kept in the labels
+                        req.extensions_mut().insert::<MetricsConfig>(
+                            MetricsConfig { cardinality_keep_params: vec!["service_id".to_string()] }
+                        );
+                        srv.call(req)
+                    })
+                    .route(web::get().to(get_posts_details))
+            )
+            .service(
+                // example of a route where we want to ignore the cardinality of `post_id` in the metrics
                 web::resource("/posts/{post_id}")
                     .name("Posts endpoint")
                     .route(web::get().to(get_posts_details))
@@ -37,3 +54,4 @@ async fn main() -> std::io::Result<()> {
     .await?;
     Ok(())
 }
+
